aaa4979a77a6b7ba57e1f52d2c06938f6652cc4c
Elias <elias.staehle@gmail.com>
2023-01-23 22:29:39 -0500

    added base app

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Documentation
+
+## contributors
+
+img1: Photo by "https://unsplash.com/@whykei?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" why kei "https://unsplash.com/photos/8e2gal_GIE8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+
+img2: Photo by "https://unsplash.com/@enisyavuz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" Enis Yavuz "https://unsplash.com/photos/CsYaNzll_rA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
---
+++ components/Welcome/index.jsx
@@ -0,0 +1,12 @@
+import { Text, ImageBackground, StyleSheet, Animated } from 'react-native';
+const img1 = {
+  uri: 'https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+};
+const img2 = {
+  uri: 'https://images.unsplash.com/photo-1599082267768-4815b2ea6bd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+};
+Animated.sequence([]);
+const Home = () => {
+  return <Animated.View></Animated.View>;
+};
+export default Home;
